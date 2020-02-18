7d0d8a3c9b1c727a40aa6f7fc21a3d50cf31ee72
mehdi-ayari <60757521+mehdi-ayari@users.noreply.github.com>
2020-02-19 00:28:14 +0100

    crud reservation

--- src/com/wassalni/services/ReservationService.java
+++ src/com/wassalni/services/ReservationService.java
@@ -5,10 +5,84 @@
  */
 package com.wassalni.services;
 
+import com.wassalni.Iservices.IReservation;
+import com.wassalni.entites.Reservation;
+import com.wassalni.entites.Type;
+import com.wassalni.utilits.DataBase;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Mahdi
  */
-public class ReservationService {
+public class ReservationService implements IReservation {
+    private Connection con;
+    private Statement ste;
+
+    public ReservationService() {
+        
+        con = DataBase.getInstance().getConnection();
+    }
+
+    @Override
+    public void ajouter(Reservation r) throws SQLException {
+        ste = con.createStatement();
+        String requeteInsert = "INSERT INTO `reservation`( `destination`, `date_reservation`,`type_reservation`,`prix`, `user_id_client`, `voiture_id_voiture`) "
+                + "VALUES ('" + r.getDestination() + "','" + r.getDate_reservation() + "','Taxi',15,1,1);";
+        ste.executeUpdate(requeteInsert);
+    }
+
+    @Override
+    public boolean delete(Reservation r) throws SQLException {
+        PreparedStatement pre=con.prepareStatement("DELETE FROM `reservation` WHERE `id_res`= ?");
+        pre.setInt(1, r.getId_res());        
+        pre.executeUpdate();
+        
+        return true;
+    }
+
+    @Override
+    public boolean update(Reservation r) throws SQLException {
+        String type=String.valueOf(r.getType_reservation());
+                
+        PreparedStatement pre = con.prepareStatement("UPDATE `reservation` SET `destination`=?,`date_reservation`=?,`type_reservation`=?,`prix`=? WHERE `id_res`=?;");
+        pre.setString(1, r.getDestination());
+        pre.setString(2, r.getDate_reservation());
+        pre.setString(3,type );
+        pre.setFloat(4, r.getPrix());
+        pre.executeUpdate();
+        
+        return true;
+    }
+
+    @Override
+    public List<Reservation> readAll() throws SQLException {
+        List<Reservation> arr=new ArrayList<>();
+    ste=con.createStatement();
     
-}
+    ResultSet rs=ste.executeQuery("select * from Reservation");
+     while (rs.next()) {                
+               int id_res=rs.getInt(1);
+               String destination=rs.getString("destination");
+               String date_reservation=rs.getString(3);
+               Type type_reservation=Type.valueOf(rs.getString(5));
+               float prix=rs.getInt(6);
+               int user_id_client=rs.getInt(7);
+               int voiture_id_voiture=rs.getInt(8);
+               Reservation p=new Reservation(id_res, destination, date_reservation, type_reservation,prix, user_id_client,voiture_id_voiture);
+     arr.add(p);
+     }
+    return arr;
+    }
+    }
+    
+    
+    
+    
+
