f8c2322c815602e734a475d9e354ab5a5ebc28dc
mehdi-ayari <60757521+mehdi-ayari@users.noreply.github.com>
2020-02-19 00:28:29 +0100

    connexion base de donnée

--- src/com/wassalni/utilits/DataBase.java
+++ src/com/wassalni/utilits/DataBase.java
@@ -5,10 +5,38 @@
  */
 package com.wassalni.utilits;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Mahdi
  */
 public class DataBase {
     
+     String url = "jdbc:mysql://localhost:3306/wassalni_data_base";
+     String login = "root";
+     String pwd = "";
+    public  static DataBase db;
+    public Connection con;
+    private DataBase() {
+         try {
+             con=DriverManager.getConnection(url, login, pwd);
+             System.out.println("connexion etablie");
+         } catch (SQLException ex) {
+             System.out.println(ex);
+         }
+    }
+    
+    public Connection  getConnection()
+    {
+    return con;
+    }     
+    public static DataBase getInstance()
+    {if(db==null)
+        db=new DataBase();
+    return db;
+    }     
+     
 }
