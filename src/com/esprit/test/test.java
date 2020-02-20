f30c74e9ed3b3eecc242689598e2cb79ec78a965
mehdi-ayari <60757521+mehdi-ayari@users.noreply.github.com>
2020-02-20 01:14:10 +0100

    Update test.java

--- src/com/esprit/test/test.java
+++ src/com/esprit/test/test.java
@@ -16,6 +16,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 /**
  *
  * @author Mahdi
@@ -25,6 +26,20 @@ public class test extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+////         ReservationService ser = new ReservationService();
+////        Reservation r1 = new Reservation("mourouj 2","2/12/566",Type.valueOf("Taxi"),15.0f,1,1);
+////          Reservation r1 = new Reservation();
+////          r1.setDestination(dest);
+//        try {
+//         Interface_Res_Controller.ajouter(r);
+//         List<Reservation> list = ser.readAll();
+//            System.out.println(list);
+//            
+//        } catch (SQLException ex) {
+//            System.out.println(ex);
+//        }
+    
+    
         
         Scene scene = new Scene(root);
         
@@ -36,18 +51,6 @@ public class test extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        launch(args);
-        ReservationService ser = new ReservationService();
-        Reservation r1 = new Reservation("mourouj 2","2/12/566",Type.valueOf("Taxi"),15.0f,1,1);
-        
-        try {
-         ser.ajouter(r1);
-         List<Reservation> list = ser.readAll();
-            System.out.println(list);
-            
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        }
-    }
-    
-}
+       launch(args);
+       
+}}
