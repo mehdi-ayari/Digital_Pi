1a5facd37ec444de65e65d90a5b40f1cf23d1f24
seifjemai01 <60849011+seifjemai01@users.noreply.github.com>
2020-02-28 00:27:24 +0100

    Update ModifierReclamationController.java

--- src/com/esprit/test/ModifierReclamationController.java
+++ src/com/esprit/test/ModifierReclamationController.java
@@ -5,11 +5,22 @@
  */
 package com.esprit.test;
 
+import com.wassalni.entites.Reclamation;
+import  com.esprit.test.MenuReclamation;
+import com.wassalni.services.ReclamationService;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -18,18 +29,21 @@ import javafx.scene.control.TextField;
 /**
  * FXML Controller class
  *
- * @author user
+ * @author User
  */
 public class ModifierReclamationController implements Initializable {
 
     @FXML
-    private Button tfReturn;
-    @FXML
     private TextField fetitre;
     @FXML
     private TextArea fedesciption;
+      private int idReclamation;
+      Reclamation Rec =new Reclamation();
+            ReclamationService SA =new ReclamationService();
     @FXML
     private Label idrec;
+    @FXML
+    private Button tfReturn;
 
     /**
      * Initializes the controller class.
@@ -37,14 +51,62 @@ public class ModifierReclamationController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        
+              try {
+            afficher_detail();
+        } catch (SQLException ex) {
+            Logger.getLogger(ModifierReclamationController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void setIdToEdit(int id) {
 
+        this.idReclamation = id;
+    }
+  private void afficher_detail() throws SQLException{
+        int idd = MenuReclamation.Recl1.getId();
+        
+       this.idrec.setText(String.valueOf(idd));
+        Rec= SA.Get_Reclamation_by_id(idd);
+
+        
+        idrec.setText(String.valueOf(Rec.getId()));
+        fetitre.setText(Rec.getTitre());
+        fedesciption.setText(Rec.getDescription()); }
     @FXML
-    private void ModifierReclamation(ActionEvent event) {
+    private void ModifierReclamation(ActionEvent event) throws SQLException, IOException {
+       List<Reclamation> list = new ArrayList<>();
+     int idd = MenuReclamation.Recl1.getId();
+       
+
+                        
+         
+          Reclamation s = new Reclamation();
+          ReclamationService sa = new ReclamationService();
+          list=sa.Get_Reclamation_by_Id(idd);
+          s=list.get(0);
+          
+        s.setTitre(fetitre.getText());
+        s.setDescription(fedesciption.getText());
+        
+        
+        
+        
+        ReclamationService sp = new ReclamationService();
+        try {
+            sp.ModifierReclamation(s);
+        } catch (SQLException ex) {
+        }
+        Parent root1 = FXMLLoader.load(getClass().getResource("AfficherReclamation.fxml"));
+      
+      
+          tfReturn.getScene().setRoot(root1);
     }
 
     @FXML
-    private void Retour(ActionEvent event) {
+    private void Retour(ActionEvent event) throws IOException {
+        Parent root1 = FXMLLoader.load(getClass().getResource("AfficherReclamation.fxml"));
+                  tfReturn.getScene().setRoot(root1);
     }
     
 }
